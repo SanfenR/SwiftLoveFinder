c6971f21bacd7321071294e2985132c25fd731d9
sanfen.mz <mazhou@likingfit.com>
2017-08-24 19:03:59 +0800

    修改显示

--- LoveFinder/ViewController.swift
+++ LoveFinder/ViewController.swift
@@ -9,19 +9,47 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var sex: UISegmentedControl!
+    @IBOutlet weak var birth: UIDatePicker!
+    @IBOutlet weak var heigthLab: UILabel!
+    @IBOutlet weak var info: UITextView!
+    @IBOutlet weak var height: UISlider!
 
     override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        name.delegate = self
     }
 
 
     override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
+    @IBAction func heigthValueChanged(_ sender: Any) {
+        let senderUi = sender as! UISlider
+        heigthLab.text = "\(Int(senderUi.value))cm"
+    }
+   
+    @IBAction func submit(_ sender: Any) {
 
+        let gen = sex.selectedSegmentIndex == 0 ? "高富帅" : "白富美"
+
+        let gregorian = Calendar(identifier:Calendar.Identifier.gregorian)
+        let now = Date()
+        let components = gregorian.dateComponents([.year], from: birth.date, to: now)
+        let age = components.year
+
+        info.text = name.text! + gen + "\(age)" + heigthLab.text!
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
